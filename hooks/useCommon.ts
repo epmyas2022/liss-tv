c9602f54d0c91cb4561d961dc335fae9ce6bdfe0
Isaac Castillo <isaac.castillo62055@uped.edu.sv>
2026-09-15 18:08:45 -0600

    fix: add MiTV to isSmartTV user agent detection

--- hooks/useCommon.ts
+++ hooks/useCommon.ts
@@ -13,7 +13,7 @@ export function useCommon() {
   const isSmartTV = () => {
     if (typeof navigator === "undefined") return false;
     const userAgent = navigator.userAgent;
-    return /smart-tv|smarttv|appletv|googletv|hbbtv|pov_tv|netcast.tv|viera|nettv|roku|tizen|webos|philips|sony|panasonic|sharp|toshiba|hisense|aftb|afts|firetv|fire_tv|xiaomi|mibox|mitv|total_tv|linux armv|crkey|x86_64|android_tv|sdk_gphone/i.test(
+    return /smart-tv|smarttv|appletv|googletv|hbbtv|pov_tv|netcast.tv|viera|nettv|roku|tizen|webos|philips|sony|panasonic|sharp|toshiba|hisense|aftb|afts|firetv|fire_tv|xiaomi|mibox|mitv|total_tv|linux armv|crkey|x86_64|android_tv|sdk_gphone|MiTV/i.test(
       userAgent,
     );
   };
